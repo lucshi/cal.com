c43ea5e9c0ccd0543a7526d1cdb9c39b5226db0c
Shi Lei A <lei.a.shi@intel.com>
2023-07-04 21:47:52 -0700

    add replace_files/next.js next-serverjs

--- setup_ssr.sh
+++ setup_ssr.sh
@@ -8,3 +8,7 @@ yarn workspace @calcom/prisma db-migrate
 yarn workspace @calcom/prisma db-deploy
 
 yarn build
+
+# replace files
+cp replace_files/next.js ./node_modules/next/dist/server/next.js
+cp replace_files/next-server.js ./node_modules/next/dist/server/next-server.js
